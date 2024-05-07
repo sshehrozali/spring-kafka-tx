d06d96b0b55ef926fa12151b382f211322cc59b7
Shehroz Ali <sshehroza13@gmail.com>
2024-05-08 01:05:46 +0500

    Fixed incorrect dependency passing in param Created new API test class

--- src/main/kotlin/com/bank/app/config/KafkaConfig.kt
+++ src/main/kotlin/com/bank/app/config/KafkaConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
@@ -20,7 +21,7 @@ class KafkaConfig(@Value("\${kafka.bootstrap-servers}") private val bootstrapSer
                   @Value("\${kafka.transaction-id}") private val kafkaTransactionId: String) {
 
   @Bean("transferConfirmedEventProducerFactory")
-  fun transferConfirmedEventProducerFactory(producerFactory: ProducerFactory<String, TransferConfirmed>): ProducerFactory<String, TransferConfirmed> {
+  fun transferConfirmedEventProducerFactory(): ProducerFactory<String, TransferConfirmed> {
     val configProps: MutableMap<String, Any> = HashMap()
     configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
     configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
---
+++ src/test/kotlin/com/bank/app/integration/api/PostV1TransferConfirmAPITest.kt
@@ -0,0 +1,7 @@
+package com.bank.app.integration.api
+
+import org.junit.jupiter.api.DisplayName
+
+@DisplayName("POST v1/transfer/confirm API Test")
+class PostV1TransferConfirmAPITest {
+}
